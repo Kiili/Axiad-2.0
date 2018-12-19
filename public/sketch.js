7063c3d0ce16e5fe03a9cea95f53a2ceb80305aa
Mart Kaasik <mart.kaasik@gmail.com>
2018-12-20 00:13:46 +0200

    Added graph to the stocks and made color generator

--- public/sketch.js
+++ public/sketch.js
@@ -1,4 +1,3 @@
-
 //TODO, draw graphs with array "data[]"
 
 /*
@@ -13,11 +12,17 @@ Kuo tahad näha milline data[] välja näeb ss line 30
 */
 
 var ready = false;
+var width = screen.width / 2
+var height = screen.height / 2
+let myCanvas
 
 function setup() { //cannot see data[];
-    createCanvas(screen.width / 2, screen.height / 2);
-    background(255, 122, 246);
+    myCanvas = createCanvas(displayWidth / 3 * 2 - 100, displayHeight - 200);
+    myCanvas.position(displayWidth / 3, 0)
+    background(51, 51, 51);
+    colorMode(RGB)
     noLoop(); //only draws when new data is recieved
+
 }
 
 function drawGraphs() { //called in HTMLfunctions.js(17) when new data is recieved
@@ -26,12 +31,46 @@ function drawGraphs() { //called in HTMLfunctions.js(17) when new data is reciev
 }
 
 function draw() {
+    background(51, 51, 51);
+
+    // grid
+    fill(102)
+    stroke(102)
+    strokeWeight(1)
+    for (var i = 0; i < height / 40; i++) {
+        line(0, i * 40, width - 30, i * 40)
+    }
+
+    // price scale
+    for (var i = 0; i < height / 40; i++) {
+        text((height - i * 40) / 2, width - 25, i * 40 + 5)
+    }
+
+
+    fill(204)
+    stroke(204)
+    colorMode(HSB, 255)
     if (data.length > 0) {
-        //console.log(data);
-
-
-
-
+        console.log(data);
+        for (var stock = 0; stock < data.length; stock++) {
+            var name = data[stock][0]
+            var stockHUE = map(parseInt(name.charAt(0), 36) - 9, 0, 26, 0, 255)
+            noFill()
+            stroke(stockHUE, 200, 200, 180)
+            strokeWeight(3)
+            beginShape()
+            for (var j = 99; j > 0; j--) {
+                if (data[stock][3][j][1] != "not_loaded") {
+                    var price = 1 * data[stock][3][99-j][1]
+                    vertex(j * (width - 50) / 100, height - price * 2)
+                }
+            }
+            endShape()
+            strokeWeight(1)
+            stroke(stockHUE, 255, 255, 255)
+            text(name, 10, height - (1 * data[stock][3][99][1]) * 2)
+        }
 
     }
+    colorMode(RGB)
 }
